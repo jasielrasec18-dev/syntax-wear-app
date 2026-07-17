40b4b437f8fae285eb5ca52ac0ad098cac50cde0
Jasiel Rasec <jasielrasec18@gmail.com>
2026-07-17 12:10:58 -0300

    feat: adiciona componente de header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header"
+
 function App() {
 
     return (
         <>
-        <p className="bg-error">Hello World</p>
+        <Header />
         </>
     )
 }
