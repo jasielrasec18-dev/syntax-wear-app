a20ec912d21190861cb3c938036f5e10abcf6166
Jasiel Rasec <jasielrasec18@gmail.com>
2026-07-20 20:12:31 -0300

    feat: adiciona seção de categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
+import { Categories } from "./components/Categories";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main className="py-10">
         <Hero />
+        <Categories />
       </main>
     </>
   );
