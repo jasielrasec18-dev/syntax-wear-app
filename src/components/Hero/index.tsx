cdc88b7e1e5462f81644af494980baaf851279cd
Jasiel Rasec <jasielrasec18@gmail.com>
2026-08-04 09:44:12 -0300

    feat: adiciona componente de overlay

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/assets/images/banner.jpg";
 import { Button } from "../Button";
+import { Overlay } from "../Overlay";
 
 export const Hero = () => {
   return (
@@ -10,25 +11,15 @@ export const Hero = () => {
           alt="Homem sentado com os tênis da SyntaxWear"
           className="w-full h-full object-cover rounded-[20px]"
         />
-        <div className="absolute  w-full bottom-0 flex justify-end items-center text-center px-6 md:px-24 pb-32">
-          <div className="flex flex-col items-center text-white w-[388px]">
-            <h2 className="text-xl font-medium leading-normal tracking-wider mb-2.5">
-              Krypto One
-            </h2>
-            <h1 className="text-2xl leading-9 tracking-widest mb-10">
-              Transforme qualquer passo em presença
-            </h1>
 
-            <div className="flex gap-3.5">
-              <Button variant="secondary" size="sm">
-                Ver modelos
-              </Button>
-              <Button variant="primary" size="md">
-                Comprar
-              </Button>
-            </div>
-          </div>
-        </div>
+        <Overlay
+          title="Kripton One"
+          subtitle="Transforme qualquer passo em presença"
+          className="bottom-0 px-6 md:px-24 pb-24 justify-end md:items-end"
+        >
+          <Button variant="secondary">Ver modelos</Button>
+          <Button>Comprar</Button>
+        </Overlay>
       </section>
     </div>
   );
