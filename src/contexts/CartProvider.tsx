9ac937d31463aeadd9da566474538b41bfaa29b4
Jasiel Rasec <jasielrasec18@gmail.com>
2026-08-13 09:52:56 -0300

    feat: adiciona lógica de armazenar e remover produtos do local storage

--- src/contexts/CartProvider.tsx
+++ src/contexts/CartProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Product } from "../interfaces/product";
 import { CartContext } from "./CartContext";
 
@@ -10,8 +10,17 @@ export interface ProductCart extends Product {
   quantity: number;
 }
 
+const localStorageKey = "@SyntaxWear:cart";
+
 export const CartProvider = ({ children }: CartProviderProps) => {
-  const [cart, setCart] = useState<ProductCart[]>([]);
+  const [cart, setCart] = useState<ProductCart[]>(() => {
+    const cartFromLocalStorage = localStorage.getItem(localStorageKey);
+    return cartFromLocalStorage !== null ? JSON.parse(cartFromLocalStorage) : []
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(cart));
+  }, [cart]);
 
   function addToCart(product: Product): void {
     const productExistsInCart = cart.find(
@@ -73,15 +82,10 @@ export const CartProvider = ({ children }: CartProviderProps) => {
         addToCart,
         removeFromCart,
         incrementInCart,
-        decrementInCart
+        decrementInCart,
       }}
     >
       {children}
     </CartContext.Provider>
   );
 };
-
-// - adicionar um produto ao carrinho
-// - remover um produto do carrinho
-// - incrementar a quantidade de um produto
-// - decrementar a quantidade de um produto
