66366447196106526f55e2a7185bb58c9ff509cb
Jasiel Rasec <jasielrasec18@gmail.com>
2026-09-16 15:24:29 -0300

    feat: adiciona sigout da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./router-tree-gen";
 import { CartProvider } from "./contexts/CartContext/CartProvider";
 import { AuthProvider } from "./contexts/AuthContext/AuthProvider";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createRouter({ routeTree });
 
@@ -13,11 +14,13 @@ declare module "@tanstack/react-router" {
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <RouterProvider router={router} />
-      </CartProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={import .meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <AuthProvider>  
+        <CartProvider>
+          <RouterProvider router={router} />
+        </CartProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
 
--- src/components/MenuMobile/index.tsx
+++ src/components/MenuMobile/index.tsx
@@ -1,10 +1,12 @@
 import IconMenu from "@/assets/images/icone-menu.png";
-import { Link } from "@tanstack/react-router";
-import { useState } from "react";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { use, useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 
 import type { NavLink } from "../Header";
 import { IoMdClose } from "react-icons/io";
+import { useAuth } from "../../contexts/AuthContext/AuthContext";
+import { PiSignOutLight } from "react-icons/pi";
 
 interface MenuMobileProps {
   navLinks: NavLink[];
@@ -12,6 +14,16 @@ interface MenuMobileProps {
 
 export const MenuMobile = ({ navLinks }: MenuMobileProps) => {
   const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+
+  const { isAuthenticated, user, signOut } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Erro ao fazer sign out:", error);
+    }
+  }
 
   return (
     <>
@@ -26,7 +38,6 @@ export const MenuMobile = ({ navLinks }: MenuMobileProps) => {
         className={`${menuIsOpen ? "bg-black/70 visible" : "bg-transparent invisible"} fixed top-0 bottom-0 left-0 right-0 z-30 transition-all duration-600 ease-in-out`}
         onClick={() => setMenuIsOpen(!menuIsOpen)}
       >
-        
         <div
           className={`${menuIsOpen ? "translate-x-0" : "-translate-x-full"} absolute top-0 bottom-0 bg-white pt-6 transition-all duration-500 ease-in-out w-full`}
           onClick={(e) => e.stopPropagation()}
@@ -35,7 +46,9 @@ export const MenuMobile = ({ navLinks }: MenuMobileProps) => {
             <nav className="flex justify-between">
               <Link to="/sign-in" className="flex items-center gap-3">
                 <FaRegUserCircle className="h-6 w-6" />
-                <p>Olá! Faça seu login</p>
+                
+                { isAuthenticated ? <p>Olá, {user?.firstName}</p> : <p>Olá! Faça seu login</p> }
+                
               </Link>
               <IoMdClose className="cursor-pointer text-2xl" onClick={() => setMenuIsOpen(!menuIsOpen)} />
             </nav>
@@ -58,6 +71,17 @@ export const MenuMobile = ({ navLinks }: MenuMobileProps) => {
             <li>
               <Link to="/about" onClick={() => setMenuIsOpen(!menuIsOpen)}>Sobre</Link>
             </li>
+
+            { 
+              isAuthenticated && (
+                <li>
+                  <button onClick={handleSignOut} className="cursor-pointer hover:opacity-70 transition-opacity flex items-center gap-2">
+                    Sair
+                    <PiSignOutLight className="w-6 h-6"></PiSignOutLight>
+                  </button>
+                </li>
+              )
+            }
           </ul>
         </div>
       </div>
